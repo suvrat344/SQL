47b8de2190668fa13ddcac5ced0e02ed408d74c2
Suvrat344 <chauhansuvrat344@gmail.com>
2024-05-02 00:17:08 +0530

    Case Study On Shark Tank India

---
+++ SharkTankIndia/SharkTankIndiaCaseStudy.sql
@@ -0,0 +1,42 @@
+-- 1. You Team must promote shark Tank India season 4, The senior come up with the idea to show highest funding domain wise so that new 
+-- startups can be attracted, and you were assigned the task to show the same.
+
+
+-- 2. You have been assigned the role of finding the domain where female as pitchers have female to male pitcher ratio >70%
+
+
+-- 3. You are working at marketing firm of Shark Tank India, you have got the task to determine volume of per season sale pitch made, 
+-- pitches who received offer and pitches that were converted. Also show the percentage of pitches converted and percentage of pitches 
+-- entertained.
+
+
+-- 4. As a venture capital firm specializing in investing in startups featured on a renowned entrepreneurship TV show, you are determining 
+-- the season with the highest average monthly sales and identify the top 5 industries with the highest average monthly sales during that 
+-- season to optimize investment decisions?
+
+
+-- 5. As a data scientist at our firm, your role involves solving real-world challenges like identifying industries with consistent 
+-- increases in funds raised over multiple seasons. This requires focusing on industries where data is available across all three seasons. 
+-- Once these industries are pinpointed, your task is to delve into the specifics, analyzing the number of pitches made, offers received, 
+-- and offers converted per season within each industry.
+
+
+-- 6. Every shark wants to know in how much year their investment will be returned, so you must create a system for them, where shark will
+-- enter the name of the startup’s and the based on the total deal and equity given in how many years their principal amount will be returned
+-- and make their investment decisions.
+
+
+-- 7. In the world of startup investing, we're curious to know which big-name investor, often referred to as "sharks," tends to put the most
+-- money into each deal on average. This comparison helps us see who's the most generous with their investments and how they measure up 
+-- against their fellow investors.
+
+
+-- 8. Develop a stored procedure that accepts inputs for the season number and the name of a shark. The procedure will then provide detailed 
+-- insights into the total investment made by that specific shark across different industries during the specified season. Additionally, it
+-- will calculate the percentage of their investment in each sector relative to the total investment in that year, giving a comprehensive 
+-- understanding of the shark's investment distribution and impact.
+
+
+-- 9. In the realm of venture capital, we're exploring which shark possesses the most diversified investment portfolio across various 
+-- industries. By examining their investment patterns and preferences, we aim to uncover any discernible trends or strategies that may 
+-- shed light on their decision-making processes and investment philosophies.
