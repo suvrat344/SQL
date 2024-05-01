6c4c75bc550c468e0102d4d21b7c362701fcd862
Suvrat344 <chauhansuvrat344@gmail.com>
2024-05-01 11:30:43 +0530

    Subquery

--- Subquery.sql
+++ Subquery.sql
@@ -10,13 +10,17 @@ SELECT
 FROM
     Olympics
 WHERE
-    Medal = 'Gold' AND Year = 2008
-        AND Height > (SELECT 
+    Medal = 'Gold' 
+		AND 
+	Year = 2008
+        AND 
+	Height > (SELECT 
             AVG(Height)
         FROM
             Olympics
         WHERE
             Year = 2008);
+
 
 -- 2. Display the names of athletes who won a medal in the sport of basketball in the 2016 Olympics and whose weight is less than the 
 -- average weight of all athletes who won a medal in the 2016 Olympics.
@@ -25,14 +29,21 @@ SELECT
 FROM
     Olympics
 WHERE
-    Sport = 'BasketBall' AND Year = 2016
-        AND Medal <> ''
-        AND Weight < (SELECT 
+    Sport = 'BasketBall' 
+		AND 
+	Year = 2016
+        AND 
+	Medal <> ''
+        AND 
+	Weight < (SELECT 
             AVG(Weight)
         FROM
             Olympics
         WHERE
-            Year = 2016 AND Medal <> '');
+            Year = 2016 
+				AND 
+			Medal <> '');
+
 
 -- 3. Display the names of all athletes who have won a medal in the sport of swimming in both the 2008 and 2016 Olympics.
 SELECT DISTINCT
@@ -43,28 +54,44 @@ FROM
     FROM
         Olympics
     WHERE
-        Year = 2008 AND Medal <> ''
-            AND Sport = 'Swimming') AS t1
+        Year = 2008 
+			AND 
+		Medal <> ''
+            AND 
+		Sport = 'Swimming') AS t1
         INNER JOIN
     (SELECT 
         *
     FROM
         Olympics
     WHERE
-        Year = 2016 AND Medal <> ''
-            AND Sport = 'Swimming') AS t2 ON t1.Name = t2.Name
-        AND t1.country = t2.country;
+        Year = 2016 
+			AND 
+		Medal <> ''
+            AND 
+		Sport = 'Swimming') AS t2 ON t1.Name = t2.Name
+			AND 
+        t1.country = t2.country;
+
 
 -- 4. Display the names of all countries that have won more than 50 medals in a single year.
 SELECT 
-    country, year, COUNT(*)
+    country,
+    year,
+    COUNT(*)
 FROM
     Olympics
 WHERE
     medal <> ''
-GROUP BY country , year
-HAVING COUNT(*) > 50
-ORDER BY year , country;
+GROUP BY 
+	country,
+    year
+HAVING 
+	COUNT(*) > 50
+ORDER BY 	
+	year,
+    country;
+
 
 -- 5. Display the names of all athletes who have won more than one medals in same sport in the same year.
 SELECT DISTINCT
@@ -78,8 +105,12 @@ WHERE
             Olympics
         WHERE
             medal <> ''
-        GROUP BY id , year , sport
-        HAVING COUNT(*) > 1);
+        GROUP BY 
+			id,
+			year,
+			sport
+        HAVING 
+			COUNT(*) > 1);
 
 SELECT * FROM insurance;
 
@@ -95,8 +126,12 @@ WHERE
         FROM
             insurance
         WHERE
-            smoker = 'Yes' AND children >= 1
-                AND region = 'southeast');
+            smoker = 'Yes' 
+				AND 
+			children >= 1
+                AND 
+			region = 'southeast');
+
 
 -- 7. How many patients have claimed more than the average claim amount for patients who are not smokers and have a BMI greater than the 
 -- average BMI for patients who have at least one child?
@@ -111,12 +146,14 @@ WHERE
             insurance
         WHERE
             smoker = 'No'
-                AND bmi > (SELECT 
+                AND 
+			bmi > (SELECT 
                     AVG(bmi)
                 FROM
                     insurance
                 WHERE
                     children >= 1));
+
 
 -- 8. How many patients have claimed more than the average claim amount for patients who have a BMI greater than the average BMI for 
 -- patients who are diabetic, have at least one child, and are from the southwest region?
@@ -135,8 +172,12 @@ WHERE
                 FROM
                     insurance
                 WHERE
-                    diabetic = 'Yes' AND children >= 1
-                        AND region = 'southwest'));
+                    diabetic = 'Yes' 
+						AND 
+					children >= 1
+                        AND 
+					region = 'southwest'));
+
 
 -- 9. What is the difference in the average claim amount between patients who are smokers and patients who are non-smokers, and have the 
 -- same BMI and number of children?
